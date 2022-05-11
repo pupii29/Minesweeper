1668fe625dee5a1a48af9546a6edd650277a3381
vtenpo <phongzed2001@gmail.com>
2022-05-11 21:53:50 +0700

    commit

--- Minesweeper/src/minesweeper/Board.java
+++ Minesweeper/src/minesweeper/Board.java
@@ -1,7 +1,25 @@
 package minesweeper;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Board extends JPanel {
 
+    private static JLabel statusbar;
+    
+    private int nCols;
+    private int nRows;
+    private int nMines;
+
+    public Board(JLabel statusbar, int nRow, int nCol, int nMines) {
+        this.statusbar = statusbar;
+        this.nRows = nRows;
+        this.nCols = nCol;
+        this.nMines = nMines;
+        initBoard();
+    }
+
+    public void initBoard() {
+
+    }
 }
