29504742851fca4c1a1ab8b65b5debd869407327
pupii29 <67531765+pupii29@users.noreply.github.com>
2022-06-04 20:40:31 +0700

    Fixxx

---
+++ Java-Minesweeper-Game-master/src/com/Minesweeper.java
@@ -0,0 +1,33 @@
+
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.EventQueue;
+
+public class Minesweeper extends JFrame {
+    private JLabel statusbar;
+
+    public Minesweeper() {
+        initUI();
+    }
+
+    public void initUI() {
+    	statusbar = new JLabel("");
+        add(statusbar, BorderLayout.SOUTH);
+        add(new Board(statusbar));
+        setResizable(false);
+        pack();
+        setTitle("Minesweeper");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            var ex = new Minesweeper();
+            ex.setVisible(true);
+        });
+        PlayMusic.playMusic("C://Users//DELL//Documents//GitHub//Minesweeper//Minesweeper//src//sound//theme.wav");
+    }
+}
---
+++ Java-Minesweeper-Game-master/src/com/PlayMusic.java
@@ -0,0 +1,37 @@
+
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class PlayMusic {
+    public static void playMusic(String path){
+        try {
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path));
+            Clip clip = AudioSystem.getClip();
+            clip.open(inputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void playSound(String path){
+        try {
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path));
+            Clip clip = AudioSystem.getClip();
+            clip.open(inputStream);
+            clip.loop(0);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
